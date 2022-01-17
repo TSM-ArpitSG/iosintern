cf84bb58952de7d8bd46724bc730ddef4041e90f
TSM-ArpitSG <66996017+TSM-ArpitSG@users.noreply.github.com>
2022-01-18 00:56:02 +0530

    git-upload
    Debugged code

---
+++ compass-Secure/CompassView_SwiftUI-main/CompassSwiftUI/CompassSwiftUI/CompassHeading.swift
@@ -0,0 +1,44 @@
+//
+//  CompassHeading.swift
+//  CompassSwiftUI
+//
+//  Created by Anthony Codes on 12/10/2019.
+//  Copyright © 2019 Anthony Codes. All rights reserved.
+//
+
+import Foundation
+import Combine
+import CoreLocation
+import CoreMotion
+
+class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    var degrees: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.setup()
+    }
+    
+    private func setup() {
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.headingAvailable() {
+            self.locationManager.startUpdatingLocation()
+            self.locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.degrees = -1 * newHeading.magneticHeading
+    }
+}
